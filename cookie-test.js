ed306001098393feb937396f03beb39a3d503dd1
ENZ0g <48256942+ENZ0g@users.noreply.github.com>
2019-09-23 21:37:53 +0300

    Add files via upload

--- cookie-test.js
+++ cookie-test.js
@@ -8,4 +8,6 @@ city.oninput = () => {
 if (document.cookie) {
     city.value = document.cookie.split('=')[1];
     city.readOnly = 1;
+    reset_btn.display = 'block';
+    reset_btn.onclick = () => document.cookie = 'city=';
 };
