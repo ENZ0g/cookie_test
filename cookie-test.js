b443878589df40c7d287072cf88999fac99f5866
ENZ0g <48256942+ENZ0g@users.noreply.github.com>
2019-09-23 22:22:11 +0300

    Add files via upload

--- cookie-test.js
+++ cookie-test.js
@@ -11,9 +11,15 @@ if (current_city) {
     city.value = decodeURIComponent(current_city);
     city.readOnly = 1;
     reset_btn.style.display = 'block';
+    li_1.style.color = '#e9ecef';
+    li_2.style.color = '#e9ecef';
+    li_3.style.display = 'list-item';
     reset_btn.onclick = () => {
         document.cookie = '';
         city.readOnly = 0;
         reset_btn.style.display = 'none';
+        li_1.style.color = '#212529';
+        li_2.style.color = '#212529';
+        li_3.style.display = 'none';
     }
 };
